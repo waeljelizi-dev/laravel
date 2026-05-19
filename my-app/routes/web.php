b7d3ecf594e5f951e6991cb7d025702ea4776a5a
Wael Jelizi <58390174+waeljelizi-dev@users.noreply.github.com>
2026-05-19 20:31:58 +0000

    second version

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+//a simple closure route (good for quick testing)
+Route::get('/', function(){
     return view('welcome');
 });
+
+//resource routes - one line of code to create all the 7 CRUD routes
+Route::resource('posts', PostController::class);
+
+//Named routes - you can give a name to a route and use that name in your code instead of the URL
+Route::get('/about', function(){
+    return view('about');
+})->name('about');
+
+
