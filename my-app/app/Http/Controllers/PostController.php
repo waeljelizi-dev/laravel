446558e170945a408482fd65d1913636ad87ebfa
Wael Jelizi <58390174+waeljelizi-dev@users.noreply.github.com>
2026-05-20 11:33:40 +0000

    PostController version request

--- my-app/app/Http/Controllers/PostController.php
+++ my-app/app/Http/Controllers/PostController.php
@@ -11,7 +11,15 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        //hardcode data 
+        $posts = [
+            ['id'=>1, 'title'=> 'Getting Started with Laravel','body'=>'Laravel makes PHP enjoyable...'],
+            ['id'=>2, 'title'=>'Understanding MVC','body'=>'The Model-View-Controller pattern...'],
+            ['id'=>3, 'title'=>'Blade Templates','body'=>'Blade is Laravel\'s templating engine...']
+        ];
+
+        //pass data to view using compact function
+        return view('posts.index', compact('posts'));
     }
 
     /**
@@ -31,11 +39,20 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * GET /posts/{id} - Show a specific post
      */
     public function show(string $id)
-    {
-        //
+    {   
+        //we'll replace this function by Post::findOrFail($id) once we have a model and database
+        $post_specific = [
+            'id' => $id,
+            'title' => "Sample Post $id",
+            'body' => "This is the body of post $id. It contains detailed information"
+        ];
+
+        //this will pass the $post_specific variable to the view
+        return view('posts.show',compact('post_specific'));
+    
     }
 
     /**
