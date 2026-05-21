270185f6b059f05d6006af5231c5a4e6de941e25
Wael Jelizi <58390174+waeljelizi-dev@users.noreply.github.com>
2026-05-21 13:33:56 +0000

    fixing error showing the posts

--- my-app/resources/views/posts/index.blade.php
+++ my-app/resources/views/posts/index.blade.php
@@ -4,9 +4,9 @@
     <h1>All Posts</h1>
     @forelse ($posts as $post)
         <div class="card">
-            <h2>{{$post->title}}</h2>
-            <p>{{Str::limit($post->body, 100)}}</p>
-            <a href="{{route('posts.show', $post->id)}}">Read More -> </a>
+            <h2>{{$post['title']}}</h2>
+            <p>{{Str::limit($post['body'], 100)}}</p>
+            <a href="{{route('posts.show', $post['id'])}}">Read More -> </a>
         </div>
     @empty
         <p>No posts yet!</p>
--- my-app/resources/views/posts/show.blade.php
+++ my-app/resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
-@section('title',$post->title)
+@section('title',$post_specific['title'])
 @section('content')
     <a href="{{route('posts.index')}}">Back to posts</a>
-    <h1>{{$post->title}}</h1>
-    <p>{{$post->body}}</p>
+    <h1>{{$post_specific['title']}}</h1>
+    <p>{{$post_specific['body']}}</p>
 @endsection
